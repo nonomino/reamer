b217c954fa69c9b8d4e4fdc7577ce39e0f116513
EugeneFelix <ontharros@gmail.com>
2023-01-16 14:51:16 +0530

    commit

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ApplicationRecord
     belongs_to :folder
     has_many :tags
+    has_many :obj_types
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ApplicationRecord
+belongs_to :document
 end
