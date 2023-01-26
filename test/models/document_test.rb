fe15dbe6bc131442c1a65fa4be4477080985c3eb
EugeneFelix <ontharros@gmail.com>
2023-01-26 13:57:32 +0530

    commit

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save document without title" do
+  article = Document.new
+  assert_not article.save, "Saved document without a Folder."
+end
 end
