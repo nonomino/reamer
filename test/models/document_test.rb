60965a2e021babd150b2f9e3b331acdf4c427831
EugeneFelix <ontharros@gmail.com>
2023-01-28 23:30:31 +0530

    commit

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class DocumentTest < ActiveSupport::TestCase
   test "should not save document without title" do
   article = Document.new
-  assert_not article.save, "Failure: Saved document without a title."
+  assert_not article.save, "Failure: Saved document without a title!"
 end
 end
